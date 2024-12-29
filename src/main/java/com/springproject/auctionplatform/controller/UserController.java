2aabb4198f482eefe7b3a544f6adbb1c8ab523a4
Plamen Atanasov <plamen.atanasov25@gmail.com>
2024-12-29 14:34:51 +0200

    Add business logic and controller endpoint for user profile

--- src/main/java/com/springproject/auctionplatform/controller/UserController.java
+++ src/main/java/com/springproject/auctionplatform/controller/UserController.java
@@ -1,11 +1,36 @@
 package com.springproject.auctionplatform.controller;
 
+import com.springproject.auctionplatform.model.entity.User;
+import com.springproject.auctionplatform.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
+    private UserService userService;
 
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
+    @GetMapping("/{username}")
+    public String getUserProfile(@PathVariable("username") String username, Model model, HttpSession session) {
+        if (session.getAttribute("username") == null ||
+            !session.getAttribute("username").equals(username)) {
+
+            return "redirect:/auth/login";
+        }
+
+        User user = userService.getUserByUsername(username);
+        model.addAttribute("user", user);
+
+        return "user-profile";
+    }
 }
