aec1c30a70f7e10dbed40d3992f57804d4a158d5
angelov00 <147129698+angelov00@users.noreply.github.com>
2025-01-02 17:02:06 +0200

    code refactoring

--- src/main/java/com/springproject/auctionplatform/service/PromotionService.java
+++ src/main/java/com/springproject/auctionplatform/service/PromotionService.java
@@ -1,17 +1,13 @@
 package com.springproject.auctionplatform.service;
 
-import com.springproject.auctionplatform.model.DTO.AuctionPreviewDTO;
 import com.springproject.auctionplatform.model.DTO.PromotionDTO;
-import com.springproject.auctionplatform.model.DTO.UserDetailsDTO;
 import com.springproject.auctionplatform.model.entity.Promotion;
 import com.springproject.auctionplatform.model.entity.User;
 import com.springproject.auctionplatform.repository.PromotionRepository;
-import com.springproject.auctionplatform.repository.UserRepository;
 import com.springproject.auctionplatform.util.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,7 +22,8 @@ public class PromotionService {
     }
 
     public List<PromotionDTO> getPromotionsByUser(User user) {
-        return this.promotionRepository.findByUser(user).stream().map(ModelMapper::convertPromotionToPromotionDTO).toList();
+
+        return this.promotionRepository.findByUser(user).stream().sorted((p1, p2) -> p2.getPromotionDate().compareTo(p1.getPromotionDate())).map(ModelMapper::convertPromotionToPromotionDTO).toList();
     }
 
     public PromotionDTO getPromotionById(Long promotionId) {
--- src/main/java/com/springproject/auctionplatform/util/PDFGenerator.java
+++ src/main/java/com/springproject/auctionplatform/util/PDFGenerator.java
@@ -3,12 +3,9 @@ package com.springproject.auctionplatform.util;
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.springproject.auctionplatform.model.DTO.PromotionDTO;
-import com.springproject.auctionplatform.model.entity.Promotion;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -16,19 +13,17 @@ import java.time.format.DateTimeFormatter;
 public class PDFGenerator {
 
     public void generateInvoice(PromotionDTO promotion, HttpServletResponse response) throws Exception {
-        // Настройки на PDF документ
+
         Document document = new Document();
         PdfWriter.getInstance(document, response.getOutputStream());
 
         document.open();
 
-        // Добавяне на заглавие на фактурата
         Font titleFont = new Font(Font.FontFamily.TIMES_ROMAN, 18, Font.BOLD);
         Paragraph title = new Paragraph("Invoice for Promoted Auction", titleFont);
         title.setAlignment(Element.ALIGN_CENTER);
         document.add(title);
 
-        // Добавяне на данни за промоцията
         Font textFont = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.NORMAL);
 
         // Заглавие
@@ -38,14 +33,11 @@ public class PDFGenerator {
         document.add(new Paragraph("Promotion Duration(in days): " + promotion.getDuration(), textFont));
         document.add(new Paragraph("Payment Method: " + promotion.getPaymentMethod().toString(), textFont));
 
-        // Форматиране на дата
-        // SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm");
         LocalDateTime promotionDate = promotion.getPromotionDate();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         String formattedDate = promotionDate.format(formatter);
         document.add(new Paragraph("Promotion Date: " + formattedDate, textFont));
 
-        // Добавяне на разделител
         document.add(new Paragraph("\n-------------------------------------\n"));
 
         // Затваряне на документа
