3dbace3d25b0ad882973e19fe8dd52d70967293d
Plamen Atanasov <plamen.atanasov25@gmail.com>
2025-02-09 10:55:39 +0200

    Add conversation creation controller endpoint

--- src/main/java/com/springproject/auctionplatform/controller/ConversationController.java
+++ src/main/java/com/springproject/auctionplatform/controller/ConversationController.java
@@ -1,12 +1,15 @@
 package com.springproject.auctionplatform.controller;
 
 import com.springproject.auctionplatform.config.security.CustomUserDetails;
+import com.springproject.auctionplatform.model.entity.Conversation;
 import com.springproject.auctionplatform.service.ConversationService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,5 +28,17 @@ public class ConversationController {
             conversationService.getParticipantsByConversationId(user.getUsername()));
 
         return "conversations";
+    }
+
+    @PostMapping
+    public String addConversation(@Valid Conversation conversation,
+                                  @AuthenticationPrincipal CustomUserDetails user) {
+        if (!conversation.getParticipants().contains(user.getUser())) {
+            throw new RuntimeException("Participant not in conversation");
+        }
+
+        conversationService.createConversation(conversation);
+
+        return "redirect:/conversations";
     }
 }
