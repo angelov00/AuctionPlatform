02c3261bd58469270a60944e0d7bce56d2781282
Plamen Atanasov <plamen.atanasov25@gmail.com>
2025-02-10 15:56:10 +0200

    Refactor code

--- src/main/java/com/springproject/auctionplatform/controller/AuctionController.java
+++ src/main/java/com/springproject/auctionplatform/controller/AuctionController.java
@@ -128,7 +128,6 @@ public class AuctionController {
         model.addAttribute("ongoingAuctions", ongoingAuctions);
         model.addAttribute("toBeFinalizedAuctions", toBeFinalizedAuctions);
         model.addAttribute("completedAuctions", completedAuctions);
-        model.addAttribute("conversationDTO", new ConversationDTO());
 
         return "my-auctions";
     }
